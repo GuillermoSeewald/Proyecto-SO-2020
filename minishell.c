f0aba71bea7c0a69b285a9ea7b6efa440507965e
Guillermo <guilleseewald@gmail.com>
2020-10-17 18:07:52 -0300

    modificaciones minishell

--- minishell.c
+++ minishell.c
@@ -1,83 +1,211 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio_ext.h>
+
 #define SIZECOMMANDS 8
 #define PATHSIZE 200
 #define COMMAND 100
-#define MAXARGUMENTS 20
+#define MAXARGUMENTS 10
 
-char path[PATHSIZE];
-char *commands[SIZECOMMANDS] = {"mkDir","rmDir","mkFile","lsDir","lsFile","perms","help","quit"};
-char *actualCommand;
-char *splitedArguments[MAXARGUMENTS];
+char *commands[SIZECOMMANDS] = {"mkdir","rmdir","mkfile","lsdir","lsfile","perms","help","quit"};
+
+void printPath();
+char* getCommand();
+void splitArguments(char* command, char* splitedArguments[]);
+int selectCommand(char* splitedArguments[]);
+
+void createDir(char* dir);
+void removeDir(char* dir);
+void createFile(char* file);
+void lsDir(char* dir);
+void lsFile(char *file);
+void changeMod(char* path, char* mode);
+void help();
 
 int main(int argc, char **argv) {
 	int exit = 0;
-
+	pid_t pid;
 	if (argc == 1) {
-		// Obtengo el directorio actual para mostrarlo en la consola
-		getcwd(path, PATHSIZE);
-
 		do {
-			// Reservo memoria para obtener el comando deseado a ejecutar
-			actualCommand = (char*) malloc(sizeof(char)*100);
+			printPath();
+			char* command = getCommand();
+			char* splitedArguments[MAXARGUMENTS];
+			splitArguments(command, splitedArguments);
+			__fpurge(stdin); // Limpia el buffer para la lectura del siguiente comando
+			selectCommand(splitedArguments);
 
-			// Imprimo el directorio actual
-			printf("\e[93m%s $ \e[0m", path);
-
-			// Obtengo el comando ingresado
-			scanf("%[^\n]s", actualCommand);
-
-			// Separo los argumentos
-			numberOfArguments = getArguments()
-
-			__fpurge(stdin);
-
-			selectCommand(numberOfArguments);
-		} while (!exit)
+			exit++;
+			printf("Ejecucion numero %d\n", exit);
+		} while (exit<3);
 	}
+	return 0;
+}
+
+void printPath() {
+	char path[PATHSIZE];
+	getcwd(path, PATHSIZE);
+	printf("\e[93m%s $ \e[0m", path);
+}
+
+char* getCommand() {
+	char* newCommand = (char*) malloc(sizeof(char)*100);
+	scanf("%[^\n]s", newCommand);
+	return newCommand;
 }
 
 /*
  * Split a string into the arguments of the sent command
  */
-int splitArguments() {
+void splitArguments(char* command, char* splitedArguments[]) {
 	int i = 0;
-	splitedArguments[0] = strtok(actualCommand, " ");
-
+	splitedArguments[0] = strtok(command, " ");
 	while (i < MAXARGUMENTS && splitedArguments[i] != NULL) {
 		i++;
-		splitedArguments[i] = strtol(NULL, " ");
+		splitedArguments[i] = strtok(NULL, " ");
 	}
-
-	return i;
 }
 
-void selectCommand(int numberOfArguments) {
-	switch(splitedArguments[0]) {
-		case(commands[0]): {
+int selectCommand(char* splitedArguments[]) {
+	int exit = 0;
+	lsDir(splitedArguments[1]);
+	/*
+	int i = 0;
+	while (i<SIZECOMMANDS && !strcmp(commands[i], splitedArguments[0])) {
+		i++;
+	}
+	switch(i) {
+		case(0): {
+		// Crear directorio
+			scanf("%s",dir)
+			createDir(dir);
 			break;
 		}
-		case(commands[1]): {
+		case(1): {
+		//Eliminar directorio
+		  scanf("%s",dir)
+			removeDir(dir);
 			break;
 		}
-		case(commands[2]): {
+		case(2): {
+		//Crear archivo
+			scanf("%s",file)
+			createFile(file);
 			break;
 		}
-		case(commands[3]): {
+		case(3): {
+		//Listar Directorio
+			scanf("%s",dir)
+			lsDir(dir);
 			break;
 		}
-		case(commands[4]): {
+		case(4): {
+		//Listar Archivo
+			scanf("%s",arc)
+			lsFile(arc)
 			break;
 		}
-		case(commands[5]): {
+		case(5): {
+		//Cambiar permisos
+			scanf("%s",path)
+			scanf("%s",modo)
+			changeMod(path,modo);
 			break;
 		}
-		case(commands[6]): {
+		case(6): {
+		//Mostrar ayuda del shell
+			help();
 			break;
 		}
-		case(commands[7]): {
+		case(7): {
+			exit = 1
+			//free malloc tendria q hacer
 			break;
 		}
 		default {
-			printf("Invalid arguments\n");
+			printf("Invalid command\n");
 		}
+	}*/
+	return exit;
+}
+/**
+*@brief Crea un directorio/s
+*@param  dir direccion del directorio a crear
+*/
+void createDir(char* dir){
+	 int resultCode;
+	 resultCode = mkdir(dir,0777); //creo que asi queda con los permiso por defecto
+	 if(resultCode != 0){
+		 		perror("Error al crear directorio %s \n");
+	 }
+}
+/**
+*@brief Elimina directorio
+*@param dir directorio a eliminar
+*/
+void removeDir(char* dir) {
+	if (rmdir(dir) == -1) {
+		perror("Remove dir");
 	}
+}
+/**
+*@brief Crea un archivo
+*@param file archivo a crear
+*/
+void createFile(char* file){
+	int resultCode;
+	resultCode = creat(file,0777);//por ahora permisos por defecto
+		if(resultCode == -1)
+			perror("Error al crear archivo");
+}
+
+
+void lsDir(char* dir) {
+	pid_t pid = fork();
+	if (pid==0){
+	char* args[] = {"ls", dir, NULL};
+	execvp("ls", args);
+exit(0);}
+}
+
+/**
+*@brief Lista el contenido de un archivo pasado por parametro
+*@param file el archivo a listar su contenido
+*/
+void lsFile(char *file){
+	//tengo que hacer referencia al archivo con el nombre de file abrirlo y imprimir los caracteres con el fopen
+		FILE *fptr;
+
+		fptr=fopen(file, "r");//Abro el archivo
+		if(fptr == NULL){
+						printf("No se pudo abrir el archivo: %s\n", file);
+		}else{
+			//El archivo se procede a leer y imprimir el contenido del mismo
+			char cont;
+			cont=fgetc(fptr);
+				while(cont != EOF){
+								printf ("%c",cont);
+								cont= fgetc(fptr);
+			}
+			fclose(fptr);
+		}
+}
+/**
+*@brief Modifica los permisos de un archivo
+*@param path Direcion del archivo
+*@param mode Permiso a cambiar
+ */
+void changeMod(char* path, char* mode) {
+	int mod = atoi(mode);
+	if (chmod(path, mod)) {
+		perror("Change mod");
+	}
+}
+
+void help() {
+	printf("Ayuda\n");
 }
