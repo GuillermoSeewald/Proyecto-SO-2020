c44cafd6440c9f6961d6bca3f94570c1ec975e75
Guillermo <guilleseewald@gmail.com>
2020-11-08 15:36:48 -0300

    clean

--- Comida rapida/fastFoodTable.c
+++ Comida rapida/fastFoodTable.c
@@ -7,37 +7,46 @@
 #include <string.h>
 #define EATING_TIME 2
 #define CLEANING_TIME 1
-#define CUSTOMERS 20
+#define CUSTOMERS 50
 #define CHEFS 3
-#define TABLES 10
+#define TABLES 30
 #define FOOD_QUEUE_SIZE 10
-#define COLUMN_SIZE 28
+#define COLUMN_SIZE 20
 #define MAX_COLUMNS 4
 
 sem_t foodQueueFull;
 sem_t foodQueueEmpty;
-sem_t cleanTables;
+sem_t cleantrles;
 sem_t orderFood;
 sem_t deliveredFood;
-sem_t dirtyTables;
+sem_t dirtytrles;
+sem_t print;
 
+char* defaultCenterText;
+
+void threads();
 void initSemaphores();
 //void destroySemaphores();
 void* chefWork(void* args);
 void* customerWork(void* args);
 void* waiterWork(void* args);
 void* cleanerWork(void* args);
-void tab(int columnNumber, char* text);
+void tableHeader();
+char* centerColumn(char* text);
+void tr(int columnNumber, char* text);
 
 int main() {
+    initSemaphores();
+    tableHeader();
+    threads();
+
+//    destroySemaphores();
+    return 0;
+}
+
+void threads() {
     pthread_t customers[CUSTOMERS], chefs[CHEFS], waiter, cleaner;
     int i;
-
-    printf("          Cliente           |          Cocinero          |           Camarero         |          Limpiador         |\n");
-    printf("--------------------------------------------------------------------------------------------------------------------\n");
-
-    initSemaphores();
-
     for (i=0; i<CUSTOMERS; i++) {
         int* id = (int*) malloc(sizeof(int));
         *id = (i+1);
@@ -59,38 +68,36 @@ int main() {
     }
     pthread_join(waiter, NULL);
     pthread_join(cleaner, NULL);
-
-//    destroySemaphores();
-    return 0;
 }
 
 void initSemaphores() {
     sem_init(&foodQueueFull, 0, 0);
     sem_init(&foodQueueEmpty, 0, FOOD_QUEUE_SIZE);
-    sem_init(&cleanTables, 0, TABLES);
+    sem_init(&cleantrles, 0, TABLES);
     sem_init(&orderFood, 0, 0);
     sem_init(&deliveredFood, 0, 0);
-    sem_init(&dirtyTables, 0, 0);
+    sem_init(&dirtytrles, 0, 0);
+    sem_init(&print, 0, 1);
 }
 
 /*
 void destroySemaphores() {
     sem_destroy(&foodQueueFull);
     sem_destroy(&foodQueueEmpty);
-    sem_destroy(&cleanTables);
+    sem_destroy(&cleantrles);
     sem_destroy(&orderFood);
     sem_destroy(&deliveredFood);
-    sem_destroy(&dirtyTables);
+    sem_destroy(&dirtytrles);
 }*/
 
 void* chefWork(void* args) {
     int id = *((int*) args);
     int columnNumber = 1;
-    char* text = (char*) malloc(sizeof(char)*50);
-    sprintf(text, "%d preparó comida", id);
+    char* text = (char*) malloc(sizeof(COLUMN_SIZE));
+    sprintf(text, "%d preparo comida", id);
     while (1) {
         sem_wait(&foodQueueEmpty);
-        tab(columnNumber, text);
+        tr(columnNumber, text);
         sem_post(&foodQueueFull);
     }
     pthread_exit(NULL);
@@ -99,47 +106,46 @@ void* chefWork(void* args) {
 void* customerWork(void* args) {
     int id = *((int*) args);
     int columnNumber = 0;
-    char* text1 = (char*) malloc(sizeof(char)*50);
-    char* text2 = (char*) malloc(sizeof(char)*50);
-    char* text3 = (char*) malloc(sizeof(char)*50);
-    char* text4 = (char*) malloc(sizeof(char)*50);
-    char* text5 = (char*) malloc(sizeof(char)*50);
+    char* text1 = (char*) malloc(sizeof(COLUMN_SIZE));
+    char* text2 = (char*) malloc(sizeof(COLUMN_SIZE));
+    char* text3 = (char*) malloc(sizeof(COLUMN_SIZE));
+    char* text4 = (char*) malloc(sizeof(COLUMN_SIZE));
+    char* text5 = (char*) malloc(sizeof(COLUMN_SIZE));
     sprintf(text1, "%d espera mesa", id);
     sprintf(text2, "%d obtuvo mesa", id);
-    sprintf(text3, "%d pidió comida", id);
+    sprintf(text3, "%d pidio comida", id);
     sprintf(text4, "%d obtuvo comida", id);
-    sprintf(text5, "%d comió y se va", id);
+    sprintf(text5, "%d comio y se va", id);
     while (1) {
-        tab(columnNumber, text1);
-        sem_wait(&cleanTables);
-        tab(columnNumber, text2);
+        tr(columnNumber, text1);
+        sem_wait(&cleantrles);
+        tr(columnNumber, text2);
         sem_post(&orderFood);
-        tab(columnNumber, text3);
+        tr(columnNumber, text3);
         sem_wait(&deliveredFood);
-        tab(columnNumber, text4);
-//        sleep(EATING_TIME);
-        tab(columnNumber, text5);
-        sem_post(&dirtyTables);
+        tr(columnNumber, text4);
+        tr(columnNumber, text5);
+        sem_post(&dirtytrles);
     }
     pthread_exit(NULL);
 }
 
 void* waiterWork(void* args) {
     int columnNumber = 2;
-    char* text1 = (char*) malloc(sizeof(char)*50);
-    char* text2 = (char*) malloc(sizeof(char)*50);
-    char* text3 = (char*) malloc(sizeof(char)*50);
-    sprintf(text1, "Tomé un pedido");
-    sprintf(text2, "Obtuve comida de la cola");
-    sprintf(text3, "Entregué un pedido");
+    char* text1 = (char*) malloc(sizeof(COLUMN_SIZE));
+    char* text2 = (char*) malloc(sizeof(COLUMN_SIZE));
+    char* text3 = (char*) malloc(sizeof(COLUMN_SIZE));
+    sprintf(text1, "Tome un pedido");
+    sprintf(text2, "Obtuve comida");
+    sprintf(text3, "Entregue pedido");
 
     while (1) {
         sem_wait(&orderFood);
-        tab(columnNumber, text1);
+        tr(columnNumber, text1);
         sem_wait(&foodQueueFull);
-        tab(columnNumber, text2);
+        tr(columnNumber, text2);
         sem_post(&foodQueueEmpty);
-        tab(columnNumber, text3);
+        tr(columnNumber, text3);
         sem_post(&deliveredFood);
     }
     pthread_exit(NULL);
@@ -147,67 +153,63 @@ void* waiterWork(void* args) {
 
 void* cleanerWork(void* args) {
     int columnNumber = 3;
-    char* text1 = (char*) malloc(sizeof(char)*50);
-    char* text2 = (char*) malloc(sizeof(char)*50);
-    char* text3 = (char*) malloc(sizeof(char)*50);
-    sprintf(text1, "Esperando mesa sucia");
-    sprintf(text2, "Hay una mesa sucia");
-    sprintf(text3, "Limpié una mesa");
+    char* text1 = (char*) malloc(sizeof(COLUMN_SIZE));
+    char* text2 = (char*) malloc(sizeof(COLUMN_SIZE));
+    char* text3 = (char*) malloc(sizeof(COLUMN_SIZE));
+    sprintf(text1, "Inactivo");
+    sprintf(text2, "Limpiando mesa");
+    sprintf(text3, "Ya limpie");
 
     while (1) {
-        tab(columnNumber, text1);
-        sem_wait(&dirtyTables);
-        tab(columnNumber, text2);
-//        sleep(CLEANING_TIME);
-        tab(columnNumber, text3);
-        sem_post(&cleanTables);
+        tr(columnNumber, text1);
+        sem_wait(&dirtytrles);
+        tr(columnNumber, text2);
+        tr(columnNumber, text3);
+        sem_post(&cleantrles);
     }
     pthread_exit(NULL);
 }
 
-char* emptyColumn(int start, int end) {
-    char* emptyColumnsText = (char*) malloc(sizeof(char) * 170);
-    char defaultText[] = "\e[93m-----\e[0m\0";
-    int defaultTextSize = 5;
-    int i, j, size1, size2;
-    size1 = (COLUMN_SIZE - defaultTextSize)/2;
-    size2 = size1 + ((COLUMN_SIZE - defaultTextSize)%2);
+void tableHeader() {
+    char* headerSep = (char*) malloc(sizeof(COLUMN_SIZE));
+    int i;
+    for (i=0; i<COLUMN_SIZE; i++)
+        headerSep[i] = '-';
+    headerSep[COLUMN_SIZE] = '\0';
+    char cl[] = "Cliente";
+    char co[] = "Cocinero";
+    char ca[] = "Camarero";
+    char li[] = "Limpiador";
+    printf("|%s|%s|%s|%s|\n", headerSep, headerSep, headerSep, headerSep);
+    printf("|%s|%s|%s|%s|\n", centerColumn(cl), centerColumn(co), centerColumn(ca), centerColumn(li));
+    printf("|%s|%s|%s|%s|\n", headerSep, headerSep, headerSep, headerSep);
 
-    for (i=start; i<end; i++) {
-        for (j=0; j<size1; j++)
-            strcat(emptyColumnsText, " ");
-        strcat(emptyColumnsText, defaultText);
-        for (j=0; j<size2; j++)
-            strcat(emptyColumnsText, " ");
-        strcat(emptyColumnsText, "|");
-    }
-    return emptyColumnsText;
+    char defaultText[] = "-----";
+    defaultCenterText = centerColumn(defaultText);
 }
 
-void tab(int columnNumber, char* text) {
-    char* tab = (char*) malloc(sizeof(char)*(200));
-    int i, size1, size2;
+char* centerColumn(char* text) {
+    char* column = (char*) malloc(sizeof(COLUMN_SIZE));
+    int sizeLeft, sizeRight;
+    size_t sizeText;
+    sizeText = strlen(text);
+    sizeLeft = (COLUMN_SIZE - sizeText) / 2;
+    sizeRight = sizeLeft + ((COLUMN_SIZE - sizeText) % 2);
+    sprintf(column, "%*s%s%-*s", sizeLeft, "", text, sizeRight, "");
+    return column;
+}
 
-    char* empty1 = emptyColumn(0, columnNumber);
-    tab = strcat(tab, empty1);
-    free(empty1);
-
-    size1 = (COLUMN_SIZE - strlen(text))/2;
-    size2 = size1 + ((COLUMN_SIZE - strlen(text))%2);
-    for (i=0; i<size1; i++)
-        strcat(tab, " ");
-    strcat(tab, text);
-    for (i=0; i<size2; i++)
-        strcat(tab, " ");
-    strcat(tab, "|");
-
-    char* empty2 = emptyColumn(columnNumber+1, MAX_COLUMNS);
-    strcat(tab, empty2);
-    free(empty2);
-
-    fflush(stdout);
-    printf("%s\n", tab);
-    free(tab);
-//    usleep(400000);
+void tr(int col, char* text) {
+    char format[] = "|%s|%s|%s|%s|";
+    char output[(COLUMN_SIZE+2)*MAX_COLUMNS];
+    char* toFormat[4] = {
+        defaultCenterText,
+        defaultCenterText,
+        defaultCenterText,
+        defaultCenterText
+    };
+    toFormat[col] = centerColumn(text);
+    sprintf(output, format, toFormat[0], toFormat[1], toFormat[2], toFormat[3]);
+    printf("%s\n", output);
 //    sleep(1);
 }
